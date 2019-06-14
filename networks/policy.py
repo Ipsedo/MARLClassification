f7a9562ab8afb16179cecb98e76d0c9218599e48
Ipsedo <berriens@gmail.com>
2019-06-14 11:52:14 +0200

    supposition pour hidden state size LSTM action unit

--- networks/policy.py
+++ networks/policy.py
@@ -1,12 +1,28 @@
 import torch.nn as nn
+from torch import cat
 
 
 class Policy(nn.Module):
     """
-    π_θ3 : ?
+    π_θ3 : A * R^n
+    R^n : pas sûr, voir reccurents.ActionUnit
     """
-    def __init__(self):
+    def __init__(self, action_size, n: int):
         super().__init__()
 
-    def forward(self, a, h_i_caret):
-        pass
+        self.__nb_a = action_size
+        self.__n = n
+
+        self.__seq_lin = nn.Sequential(
+            nn.Linear(action_size + n, (action_size + n) * 2),
+            nn.ReLU(),
+            nn.Linear((action_size + n) * 2, 1)
+        )
+
+    def forward(self, a, h_caret_i_t_p_one):
+        assert len(a.size()) == 1, "Action must be a 1-D tensor"
+        assert len(h_caret_i_t_p_one.size()) == 1, "action unit lstm hidden state must be 1-D tensor"
+
+        x = cat((a, h_caret_i_t_p_one), dim=0)
+
+        return self.__seq_lin(x)
--- networks/recurrents.py
+++ networks/recurrents.py
@@ -10,16 +10,33 @@ class BeliefUnit(nn.Module):
 
         self.__n = n
 
-    def forward(self, u_i_t):
-        pass
+        self.__lstm = nn.LSTM(self.__n * 3, self.__n, batch_first=True)
+
+    def forward(self, h_i_t, c_i_t, u_i_t):
+        assert u_i_t.size(0) == 1, "Only one time iteration is allowed"
+
+        _, (h_i_t_p_one, c_i_t_p_one) = self.__lstm(u_i_t, (h_i_t, c_i_t))
+
+        return h_i_t_p_one, c_i_t_p_one
 
 
 class ActionUnit(nn.Module):
     """
     f_θ2 : ?
+    Supposition : R^2n * R^3n -> R^2n
+    R^2n : pas sûr
     """
-    def __init__(self) -> None:
+    def __init__(self, n: int) -> None:
         super().__init__()
 
-    def forward(self, u_i_t):
-        pass
+        self.__n = n
+
+        # TODO find hidden state size in article
+        self.__lstm = nn.LSTM(self.__n * 3, self.__n, batch_first=True)
+
+    def forward(self, h_caret_i_t, c_caret_i_t, u_i_t):
+        assert u_i_t.size(0) == 1, "Only one time iteration is allowed"
+
+        _, (h_caret_i_t_p_one, c_caret_i_t_p_one) = self.__lstm(u_i_t, (h_caret_i_t, c_caret_i_t))
+
+        return h_caret_i_t_p_one, c_caret_i_t_p_one
