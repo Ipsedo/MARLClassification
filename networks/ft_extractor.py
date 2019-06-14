aad69591100a0a716213b8e976b31f7109ef4403
Ipsedo <berriens@gmail.com>
2019-06-14 10:26:10 +0200

    added state to ft module

--- networks/ft_extractor.py
+++ networks/ft_extractor.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 class CNN_MNIST(nn.Module):
     """
-    R^f*f -> R^n
+    b_θ5 : R^f*f -> R^n
     """
     def __init__(self, f: int, n: int) -> None:
         super().__init__()
@@ -24,3 +24,21 @@ class CNN_MNIST(nn.Module):
         out = self.__seq_conv(o_i_t.unsqueeze(1))
         out = out.flatten(1, -1)
         return self.__seq_lin(out)
+
+
+class StateToFeatures(nn.Module):
+    """
+    λ_θ7 : R^d -> R^n
+    """
+    def __init__(self, d: int, n: int) -> None:
+        super().__init__()
+
+        self.__d = d
+        self.__n = n
+
+        self.__seq_lin = nn.Sequential(
+            nn.Linear(self.__d, self.__n)
+        )
+
+    def forward(self, p_i_t):
+        return self.__seq_lin(p_i_t)
