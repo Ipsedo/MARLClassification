f026df0b8d30c085fc650fe77838ca851acbad48
Ipsedo <berriens@gmail.com>
2019-06-19 12:03:53 +0200

    test other CNN

--- networks/ft_extractor.py
+++ networks/ft_extractor.py
@@ -26,6 +26,30 @@ class CNN_MNIST(nn.Module):
         return self.seq_lin(out)
 
 
+class CNN_MNIST_2(nn.Module):
+    """
+    b_θ5 : R^f*f -> R^n
+    """
+    def __init__(self, f: int, n: int) -> None:
+        super().__init__()
+
+        self.__f = f
+        self.__n = n
+
+        self.seq_conv = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3),
+        )
+
+        self.seq_lin = nn.Sequential(
+            nn.Linear(8 * ((f - 2) ** 2), self.__n)
+        )
+
+    def forward(self, o_t):
+        out = self.seq_conv(o_t.unsqueeze(1))
+        out = out.flatten(1, -1)
+        return self.seq_lin(out)
+
+
 class StateToFeatures(nn.Module):
     """
     λ_θ7 : R^d -> R^n
