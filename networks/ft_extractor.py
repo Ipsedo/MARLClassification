11efac87d5f1f036919cde33e56c834c41b84e49
Ipsedo <berriens@gmail.com>
2019-06-19 16:29:51 +0200

    add ReLU -> change rien

--- networks/ft_extractor.py
+++ networks/ft_extractor.py
@@ -13,6 +13,7 @@ class CNN_MNIST(nn.Module):
 
         self.seq_conv = nn.Sequential(
             nn.Conv2d(1, 8, kernel_size=3),
+            nn.ReLU(),
             nn.Conv2d(8, 16, kernel_size=3)
         )
 
