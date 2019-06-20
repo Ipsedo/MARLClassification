f583054fcbdf9e7f37617f15682c27beb04cd739
Ipsedo <berriens@gmail.com>
2019-06-20 10:26:22 +0200

    change policy maping

--- networks/policy.py
+++ networks/policy.py
@@ -7,26 +7,25 @@ class Policy(nn.Module):
     π_θ3 : A * R^n
     R^n : pas sûr, voir reccurents.ActionUnit
     """
-    def __init__(self, action_size, n: int):
+    def __init__(self, nb_action, n: int):
         super().__init__()
 
-        self.__nb_a = action_size
         self.__n = n
 
         self.seq_lin = nn.Sequential(
-            nn.Linear(action_size + n, (action_size + n) * 2),
+            nn.Linear(n, 64),
             nn.ReLU(),
-            nn.Linear((action_size + n) * 2, 1),
-            nn.Softmax(dim=1)
+            nn.Linear(64, nb_action),
+            nn.Softmax(dim=-1)
         )
 
-    def forward(self, a, h_caret_t_next):
-        assert len(a.size()) == 2, "Action must be a 2-D tensor"
+    def forward(self, h_caret_t_next):
+        # assert len(a.size()) == 2, "Action must be a 2-D tensor"
         assert len(h_caret_t_next.size()) == 3, "action unit lstm hidden state must be 3-D tensor"
 
-        a = stack([a] * h_caret_t_next.size(1), dim=1)
-        b = cat([h_caret_t_next.squeeze(1)] * a.size(0), dim=0)
+        #a = stack([a] * h_caret_t_next.size(1), dim=1)
+        #b = cat([h_caret_t_next.squeeze(1)] * a.size(0), dim=0)
 
-        x = cat((a, b), dim=2).permute(1, 0, 2)
+        #x = cat((a, b), dim=2).permute(1, 0, 2)
 
-        return self.seq_lin(x)
+        return self.seq_lin(h_caret_t_next).squeeze(0)
