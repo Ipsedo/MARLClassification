24e11cb5d5eb18b1639d307264da9bf3215416ee
Ipsedo <berriens@gmail.com>
2019-06-20 10:25:41 +0200

    simplifié action

--- environment/transition.py
+++ environment/transition.py
@@ -1,37 +1,17 @@
 import torch as th
 
 
-def trans_MNIST(pos: th.Tensor, a_t_p_one: th.Tensor, f: int, img_size: int) -> th.Tensor:
+def trans_MNIST(pos: th.Tensor, a_t_next: th.Tensor, f: int, img_size: int) -> th.Tensor:
     """
 
     :param pos:
-    :param a_t_p_one: Tensor of size = (4,)
-        one_hot_vector [up, down, left, right]
+    :param a_t_next: Tensor of size = (2,)
     :param f:
     :param img_size:
     :return:
     """
 
-    dir = th.zeros(pos.size(0), 2)
-
-    if pos.is_cuda:
-        dir = dir.cuda()
-
-    for i in range(pos.size(0)):
-        if a_t_p_one[i, 0]:
-            # UP
-            dir[i, 1] = -1
-        elif a_t_p_one[i, 1]:
-            # DOWN
-            dir[i, 1] = 1
-        elif a_t_p_one[i, 2]:
-            # LEFT
-            dir[i, 0] = -1
-        else:
-            # RIGHT
-            dir[i, 0] = 1
-
-    new_pos = pos + dir
+    new_pos = pos + a_t_next
 
     if (new_pos[:, 0] < 0).any() or\
             (new_pos[:, 0] + f >= img_size).any() or\
