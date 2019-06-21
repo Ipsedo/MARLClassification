c896a6f2ded228ca918a2830328cac6664b4ccf2
Ipsedo <berriens@gmail.com>
2019-06-21 14:15:10 +0200

    transition fixed

--- environment/transition.py
+++ environment/transition.py
@@ -11,12 +11,12 @@ def trans_MNIST(pos: th.Tensor, a_t_next: th.Tensor, f: int, img_size: int) -> t
     :return:
     """
 
-    new_pos = pos + a_t_next
+    new_pos = pos.clone()
 
-    if (new_pos[:, 0] < 0).any() or\
-            (new_pos[:, 0] + f >= img_size).any() or\
-            (new_pos[:, 1] < 0).any() or\
-            (new_pos[:, 1] + f >= img_size).any():
-        return pos
+    idx = (new_pos[:, 0] + a_t_next[:, 0] >= 0) * \
+          (new_pos[:, 0] + a_t_next[:, 0] + f < img_size) * \
+          (new_pos[:, 1] + a_t_next[:, 1] >= 0) * \
+          (new_pos[:, 1] + a_t_next[:, 1] + f < img_size)
+    idx = idx.unsqueeze(1).to(th.float)
 
-    return new_pos
+    return idx * (new_pos + a_t_next) + (1 - idx) * new_pos
