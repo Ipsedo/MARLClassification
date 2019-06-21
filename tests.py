e97b9a8ee68a15bb3086c3ba35c07000ebc625c9
Ipsedo <berriens@gmail.com>
2019-06-21 15:27:11 +0200

    add baseline test mnist

---
+++ tests.py
@@ -0,0 +1,90 @@
+import torch as th
+import torch.nn as nn
+from math import ceil
+from data.mnist import load_mnist
+from tqdm import tqdm
+
+
+class TestCNN(nn.Module):
+    def __init__(self, n: int) -> None:
+        super().__init__()
+
+        f = 28
+        self.__n = n
+
+        self.seq_conv = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3),
+            nn.ReLU(),
+            nn.Conv2d(8, 16, kernel_size=3),
+            nn.ReLU()
+        )
+
+        self.seq_lin = nn.Sequential(
+            nn.Linear(16 * (((f - 2) - 2) ** 2), self.__n),
+            nn.Softmax(dim=-1)
+        )
+
+    def forward(self, o_t):
+        out = self.seq_conv(o_t.unsqueeze(1))
+        out = out.flatten(1, -1)
+        return self.seq_lin(out)
+
+
+def test_mnist():
+    (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = load_mnist()
+
+    m = TestCNN(10)
+    mse = nn.MSELoss()
+
+    m.cuda()
+    mse.cuda()
+
+    optim = th.optim.SGD(m.parameters(), lr=1e-3)
+
+    batch_size = 64
+    nb_epoch = 10
+
+    nb_batch = ceil(x_train.size(0) / batch_size)
+
+    for e in range(nb_epoch):
+        sum_loss = 0
+
+        m.train()
+
+        for i in tqdm(range(nb_batch)):
+            i_min = i * batch_size
+            i_max = (i + 1) * batch_size
+            i_max = i_max if i_max < x_train.size(0) else x_train.size(0)
+            x, y = x_train[i_min:i_max, :, :].cuda(), y_train[i_min:i_max].cuda()
+
+            pred = m(x)
+
+            loss = mse(pred, th.eye(10)[y].cuda())
+
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            sum_loss += loss.item()
+        print("Epoch %d, loss = %f" % (e, sum_loss / nb_batch))
+
+        with th.no_grad():
+            nb_batch_valid = ceil(x_valid.size(0) / batch_size)
+            nb_correct = 0
+            for i in tqdm(range(nb_batch_valid)):
+                i_min = i * batch_size
+                i_max = (i + 1) * batch_size
+                i_max = i_max if i_max < x_valid.size(0) else x_valid.size(0)
+
+                x, y = x_valid[i_min:i_max, :, :].cuda(), y_valid[i_min:i_max].cuda()
+
+                pred = m(x)
+
+                nb_correct += (pred.argmax(dim=1) == y).sum().item()
+
+            nb_correct /= x_valid.size(0)
+            print("Epoch %d, accuracy = %f" % (e, nb_correct))
+
+
+if __name__ == "__main__":
+    test_mnist()
