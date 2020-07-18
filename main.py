6ab85e77f2f1efdb572ed470386a6fdd261f068f
Ipsedo <berriens@gmail.com>
2020-07-18 18:23:39 +0200

    review main -> in test

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ from environment.observation import obs_MNIST
 from environment.transition import trans_MNIST
 from environment.agent import Agent
 from environment.core import step, detailled_step
+
 from networks.models import ModelsUnion
+from networks.ft_extractor import TestCNN
+
+from data.mnist import load_mnist
+
 import torch as th
 from torch.nn import Softmax, MSELoss, NLLLoss
-from data.mnist import load_mnist
-from tqdm import tqdm
+
 from math import ceil
-import matplotlib.pyplot as plt
 from random import randint
 
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-def test_MNIST_transition():
+import argparse
+
+
+def test_mnist_transition():
+    """
+    TODO
+
+    :return:
+    :rtype:
+    """
     a_1 = th.tensor([1., 0.])
     a_2 = th.tensor([0., 1.])
     a_3 = th.tensor([-1., 0.])
@@ -34,7 +48,13 @@ def test_MNIST_transition():
     print(trans_MNIST(pos_2, a_4, 5, 28))
 
 
-def test_MNIST_obs():
+def test_mnist_obs():
+    """
+    TODO
+
+    :return:
+    :rtype:
+    """
     img = th.arange(0, 28 * 28).view(1, 28, 28)
 
     print(img)
@@ -59,6 +79,12 @@ def test_MNIST_obs():
 
 
 def test_agent_step():
+    """
+    TODO
+
+    :return:
+    :rtype:
+    """
     ag = []
 
     nb_class = 10
@@ -91,6 +117,12 @@ def test_agent_step():
 
 
 def test_core_step():
+    """
+    TODO
+
+    :return:
+    :rtype:
+    """
     ag = []
 
     nb_class = 10
@@ -153,21 +185,117 @@ def test_core_step():
                 print(n.lstm.weight_hh_l0.grad)
 
 
-def train_mnist():
-    ag = []
+######################
+# CNN test
+######################
 
-    nb_class = 10
-    img_size = 28
-    n = 64
-    f = 7
-    n_m = 12
-    d = 2
-    nb_action = 4
+def test_mnist():
+    """
+    TODO
+
+    :return:
+    :rtype:
+    """
+    (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = load_mnist()
+
+    m = TestCNN(10)
+    mse = MSELoss()
+
+    m.cuda()
+    mse.cuda()
+
+    optim = th.optim.SGD(m.parameters(), lr=1e-2)
+
     batch_size = 64
-    t = 7
-    nr = 1
+    nb_epoch = 10
 
-    cuda = True
+    nb_batch = ceil(x_train.size(0) / batch_size)
+
+    for e in range(nb_epoch):
+        sum_loss = 0
+
+        m.train()
+
+        for i in tqdm(range(nb_batch)):
+            i_min = i * batch_size
+            i_max = (i + 1) * batch_size
+            i_max = i_max if i_max < x_train.size(0) else x_train.size(0)
+            x, y = x_train[i_min:i_max, :, :].cuda(), y_train[i_min:i_max].cuda()
+
+            pred = m(x)
+
+            loss = mse(pred, th.eye(10)[y].cuda())
+
+            optim.zero_grad()
+            loss.backward()
+
+            #print("CNN_el = %d, grad_norm = %f" % (m.seq_lin[0].weight.grad.nelement(), m.seq_lin[0].weight.grad.norm()))
+
+            optim.step()
+
+            sum_loss += loss.item()
+        print("Epoch %d, loss = %f" % (e, sum_loss / nb_batch))
+
+        with th.no_grad():
+            nb_batch_valid = ceil(x_valid.size(0) / batch_size)
+            nb_correct = 0
+            for i in tqdm(range(nb_batch_valid)):
+                i_min = i * batch_size
+                i_max = (i + 1) * batch_size
+                i_max = i_max if i_max < x_valid.size(0) else x_valid.size(0)
+
+                x, y = x_valid[i_min:i_max, :, :].cuda(), y_valid[i_min:i_max].cuda()
+
+                pred = m(x)
+
+                nb_correct += (pred.argmax(dim=1) == y).sum().item()
+
+            nb_correct /= x_valid.size(0)
+            print("Epoch %d, accuracy = %f" % (e, nb_correct))
+    return m.seq_conv
+
+
+######################
+# Train - Main
+######################
+
+def train_mnist(nb_class: int, img_size: int,
+                n: int, f: int, n_m: int,
+                d: int, nb_action: int, batch_size: int, t: int,
+                nr: int,
+                nb_epoch: int, cuda: bool):
+    """
+    TODO
+
+    :param nb_class:
+    :type nb_class:
+    :param img_size:
+    :type img_size:
+    :param n:
+    :type n:
+    :param f:
+    :type f:
+    :param n_m:
+    :type n_m:
+    :param d:
+    :type d:
+    :param nb_action:
+    :type nb_action:
+    :param batch_size:
+    :type batch_size:
+    :param t:
+    :type t:
+    :param nr:
+    :type nr:
+    :param nb_epoch:
+    :type nb_epoch:
+    :param cuda:
+    :type cuda:
+    :return:
+    :rtype:
+    """
+
+    ag = []
 
     #m = ModelsUnion(n, f, n_m, d, nb_action, nb_class, test_mnist())
     m = ModelsUnion(n, f, n_m, d, nb_action, nb_class)
@@ -197,8 +325,6 @@ def train_mnist():
         params += list(net.parameters())
 
     optim = th.optim.Adam(params, lr=1e-3)
-
-    nb_epoch = 10
 
     (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = load_mnist()
     x_train, y_train = x_train[:10000], y_train[:10000]
@@ -301,7 +427,23 @@ def train_mnist():
     viz(ag, x_test[randint(0, x_test.size(0)-1)], t, sm, f)
 
 
-def viz(agents: list, one_img: th.Tensor, max_it: int, softmax: Softmax, f: int):
+def viz(agents: list, one_img: th.Tensor, max_it: int, softmax: Softmax, f: int) -> None:
+    """
+    TODO
+
+    :param agents:
+    :type agents:
+    :param one_img:
+    :type one_img:
+    :param max_it:
+    :type max_it:
+    :param softmax:
+    :type softmax:
+    :param f:
+    :type f:
+    :return:
+    :rtype:
+    """
     pred, pos = detailled_step(agents, one_img.unsqueeze(0).cuda(), max_it, softmax, True, 10)
 
     plt.imshow(one_img)
@@ -321,11 +463,107 @@ def viz(agents: list, one_img: th.Tensor, max_it: int, softmax: Softmax, f: int)
         plt.show()
 
 
+def main() -> None:
+    """
+    TODO
+
+    :return:
+    :rtype:
+    """
+    parser = argparse.ArgumentParser("Multi agent reinforcement learning for image classification - Main")
+
+    sub_parser = parser.add_subparsers()
+    sub_parser.dest = "mode"
+    sub_parser.required = True
+
+    unit_test_parser = sub_parser.add_parser("unit")
+    train_parser = sub_parser.add_parser("train")
+    test_parser = sub_parser.add_parser("test")
+    test_cnn_parser = sub_parser.add_parser("cnn")
+
+    ##################
+    # Unit test args
+    ##################
+    unit_test_choices = ["transition", "observation", "agent-step", "core-step"]
+    unit_test_parser.add_argument("-t", "--test-id", type=str, choices=unit_test_choices,
+                                  default=unit_test_choices[0], dest="test_id")
+
+    ##################
+    # Train args
+    ##################
+
+    # MNIST default params
+
+    # Image / data set arguments
+    train_parser.add_argument("--nb-class", type=int, default=10, dest="nb_class")
+    train_parser.add_argument("--img-size", type=int, default=28, dest="img_size")
+
+    # Algorithm arguments
+    train_parser.add_argument("--n", type=int, default=64)
+    train_parser.add_argument("--f", type=int, default=7)
+    train_parser.add_argument("--nm", type=int, default=2, dest="n_m")
+
+    # Environment arguments
+    train_parser.add_argument("-d", "--dim", type=int, default=2)
+    train_parser.add_argument("--nb-action", type=int, default=4, dest="nb_action")
+
+    # Training arguments
+    train_parser.add_argument("--batch-size", type=int, default=64, dest="batch_size")
+    train_parser.add_argument("--step", type=int, default=7)
+    train_parser.add_argument("--nb-epoch", type=int, default=10, dest="nb_epoch")
+    train_parser.add_argument("--cuda", action="store_true", dest="cuda")
+
+    train_parser.add_argument("--nr", type=int, default=7)
+
+    ##################
+    # Test args
+    ##################
+
+    # TODO
+
+    ##################
+    # CNN test args
+    ##################
+
+    # TODO
+
+    ###################################
+    # Main - start different mods
+    ###################################
+
+    args = parser.parse_args()
+
+    # Unit tests main
+    if args.mode == "unit":
+        # Choose between one of basic test
+        if args.test_id == "transition":
+            test_mnist_transition()
+        elif args.test_id == "observation":
+            test_mnist_obs()
+        elif args.test_id == "agent-step":
+            test_agent_step()
+        elif args.test_id == "core-step":
+            test_core_step()
+        else:
+            parser.error(f"Unrecognized unit test ID : \"{args.test_id}\", choices = {unit_test_choices}.")
+    # Train main
+    elif args.mode == "train":
+        train_mnist(args.nb_class, args.img_size,
+                    args.n, args.f, args.n_m,
+                    args.dim, args.nb_action,
+                    args.batch_size, args.step,
+                    args.nr,
+                    args.nb_epoch, args.cuda)
+    # Test main
+    elif args.mode == "test":
+        pass
+    # CNN test main
+    elif args.mode == "cnn":
+        print(test_mnist())
+        pass
+    else:
+        parser.error(f"Unrecognized mode : \"{args.mode}\" type == {type(args.mode)}.")
+
+
 if __name__ == "__main__":
-    #test_MNIST_transition()
-    #test_MNIST_obs()
-
-    #test_agent_step()
-    #test_core_step()
-
-    train_mnist()
+    main()
