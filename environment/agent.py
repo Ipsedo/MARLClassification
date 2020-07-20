all fix oklm, 3-5min per epoch on MNIST <3 Need see if test results (moves, prediction, msg) are coherent to the 60-70% precision

environment/agent.py
@@ -70,7 +70,7 @@ class MultiAgent:
         # CPU vs GPU
         self.is_cuda = False
 
-    def new_img(self, batch_size: int) -> None:
+    def new_episode(self, batch_size: int) -> None:
         """
 
         :param batch_size:
@@ -96,9 +96,8 @@ class MultiAgent:
         self.msg = [th.zeros(self.__nb_agents, batch_size, self.__n_m,
                              device=th.device("cuda") if self.is_cuda else th.device("cpu"))]
 
-        self.__log_probas = [th.log(th.tensor([1. / 4.] * batch_size * self.__nb_agents,
-                                              device=th.device("cuda") if self.is_cuda else th.device("cpu"))
-                                    .view(self.__nb_agents, batch_size))]
+        self.__log_probas = [th.log(th.ones(self.__nb_agents, batch_size,
+                                            device=th.device("cuda") if self.is_cuda else th.device("cpu"))) * 1. / 4.]
 
         self.pos = th.randint(self.__size - self.__f, (self.__nb_agents, batch_size, 2),
                               device=th.device("cuda") if self.is_cuda else th.device("cpu"))
@@ -118,10 +117,12 @@ class MultiAgent:
         o_t = self.__obs(img, self.pos, self.__f)
 
         # Feature space
+        # CNN need (N, C, W, H) not (N1, ..., N18, C, W, H)
         b_t = self.__networks.map_obs(o_t.flatten(0, 1)).view(len(self), self.__batch_size, self.__n)
 
         # Get messages
         d_bar_t_tmp = self.__networks.decode_msg(self.msg[self.__t])
+        # Mean on agent
         d_bar_t_mean = d_bar_t_tmp.mean(dim=0)
         d_bar_t = ((d_bar_t_mean * self.__nb_agents) - d_bar_t_tmp) / (self.__nb_agents - 1)
 
@@ -161,8 +162,7 @@ class MultiAgent:
             .repeat(self.__nb_agents, self.__batch_size, 1, 1)
 
         # Get action probabilities
-        action_scores = self.__networks.policy(self.__h_caret[self.__t + 1].flatten(0, 1))\
-            .view(len(self), self.__batch_size, self.__action_size)
+        action_scores = self.__networks.policy(self.__h_caret[self.__t + 1])
 
         # If random walk : pick one action with uniform probability
         # Else : greedy policy
@@ -197,12 +197,6 @@ class MultiAgent:
                                 a_t_next, self.__f,
                                 self.__size).to(th.long)
 
-    def step_finished(self) -> None:
-        """
-
-        :return:
-        :rtype:
-        """
         self.__t += 1
 
     def predict(self) -> Tuple[th.Tensor, th.Tensor]:
@@ -215,6 +209,11 @@ class MultiAgent:
         return self.__networks.predict(self.__c[self.__t]), self.__log_probas[self.__t]
 
     def cuda(self) -> None:
+        """
+
+        :return:
+        :rtype:
+        """
         self.is_cuda = True
 
         # TODO pass model union to cuda here ? better real separation NNs wrapper and rl/ma agent

main.py
@@ -99,7 +99,7 @@ def test_agent_step():
 
     img = th.rand(batch_size, 28, 28, device=th.device("cuda"))
 
-    marl_m.new_img(batch_size)
+    marl_m.new_episode(batch_size)
 
     print("First step")
     print(marl_m.pos)
@@ -329,7 +329,7 @@ def train_mnist(nb_class: int, img_size: int,
     optim = th.optim.Adam(m.parameters(), lr=1e-3)
 
     (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = load_mnist()
-    x_train, y_train = x_train[:10000], y_train[:10000]
+    x_train, y_train = x_train[:30000], y_train[:30000]
 
     nb_batch = ceil(x_train.size(0) / batch_size)
 
@@ -351,43 +351,43 @@ def train_mnist(nb_class: int, img_size: int,
             i_max = (i + 1) * batch_size
             i_max = i_max if i_max < x_train.size(0) else x_train.size(0)
 
-            losses = th.zeros(nr, len(marl_m), batch_size,
-                              device=th.device("cuda") if cuda else th.device("cpu"))
+            x, y = x_train[i_min:i_max, :, :].to(th.device("cuda") if cuda else th.device("cpu")),\
+                   y_train[i_min:i_max].to(th.device("cuda") if cuda else th.device("cpu"))
 
-            for k in range(nr):
-                x, y = x_train[i_min:i_max, :, :].to(th.device("cuda") if cuda else th.device("cpu")),\
-                       y_train[i_min:i_max].to(th.device("cuda") if cuda else th.device("cpu"))
+            # get predictions and probabilities
+            preds, log_probas = episode(marl_m, x, t, cuda, eps, nb_class)
 
-                # get predictions and probabilities
-                #preds, log_probas = episode(ag, x, t, cuda, eps, nb_class)
-                preds, log_probas = episode(marl_m, x, t, cuda, eps, nb_class)
+            # Class one hot encoding
+            y_eye = th.eye(nb_class, device=th.device("cuda") if cuda else th.device("cpu"))[y]\
+                .repeat(preds.size(0), 1, 1)
 
-                # Class one hot encoding
-                y_eye = th.eye(nb_class, device=th.device("cuda") if cuda else th.device("cpu"))[y]\
-                    .repeat(preds.size(0), 1, 1)
+            # SE Loss
+            r = -(preds - y_eye) ** 2
 
-                # SE Loss
-                r = -(preds - y_eye) ** 2
+            # Mean on one hot encoding
+            r = r.mean(dim=-1)
 
-                # Sum on one hot encoding
-                r = r.sum(dim=-1)
+            # Keep loss for future update
+            losses = log_probas * r.detach() + r
 
-                # Keep loss for future update
-                losses[k, :, :] = log_probas * r.detach() + r
+            eps *= eps_decay
 
-                eps *= eps_decay
+            # Losses mean on agents and batch
+            loss = -losses.mean()
 
-            # sum / nr -> mean all experiments
-            # sum / len(ag) -> mean all agent
-            # mean -> mean on batch
-            loss = -((losses.sum(dim=0) / nr).sum(dim=0) / len(marl_m)).mean()
-
+            # Reset gradient
             optim.zero_grad()
+
+            # Backward on compute graph
             loss.backward()
+
+            # Update weights
             optim.step()
 
+            # Update epoch loss sum
             sum_loss += loss.item()
 
+            # Update epoch gradient norm sum
             grad_norm_cnn.append(m.get_networks()[0].seq_lin[0].weight.grad.norm())
             grad_norm_pred.append(m.get_networks()[-1].seq_lin[0].weight.grad.norm())
 
@@ -533,9 +533,9 @@ def main() -> None:
                               help="Train NNs with CUDA")
     train_parser.add_argument("--nr", type=int, default=7,
                               help="Number of retry")
-    train_parser.add_argument("--eps", type=float, default=0.5, dest="eps",
+    train_parser.add_argument("--eps", type=float, default=0.7, dest="eps",
                               help="Epsilon value at training beginning")
-    train_parser.add_argument("--eps-decay", type=float, default=1.0 - 1e-4, dest="eps_decay",
+    train_parser.add_argument("--eps-decay", type=float, default=1.0 - 4e-5, dest="eps_decay",
                               help="Epsilon decay, update epsilon after each episode : "
                                    "eps = eps * eps_decay")
 

networks/policy.py
@@ -21,7 +21,7 @@ class Policy(nn.Module):
         if len(h_caret_t_next.size()) == 1:
             h_caret_t_next = h_caret_t_next.unsqueeze(0)
 
-        assert len(h_caret_t_next.size()) == 2,\
-            "action unit lstm hidden state must be 2-D tensor (curr = {})".format(len(h_caret_t_next.size()))
+        assert len(h_caret_t_next.size()) == 3,\
+            f"action unit lstm hidden state must be 2-D tensor (nb_ag, batch_size, ft), curr = {h_caret_t_next.size()}"
 
         return self.seq_lin(h_caret_t_next)

networks/recurrents.py
@@ -13,10 +13,18 @@ class BeliefUnit(nn.Module):
         self.lstm = nn.LSTMCell(self.__n * 3, self.__n)
 
     def forward(self, h_t, c_t, u_t):
-        # h_t.size() == (1, nb_agent, batch_size, hidden)
-        #assert u_t.size(1) == 1, "Only one time iteration is allowed"
-
-        #h_t, c_t = h_t[:u_t.size(0), :], c_t[:u_t.size(0), :]
+        """
+        TODO
+        *
+        :param h_t:
+        :type h_t:
+        :param c_t:
+        :type c_t:
+        :param u_t:
+        :type u_t:
+        :return:
+        :rtype:
+        """
 
         nb_ag, batch_size, hidden_size = h_t.size()
 
@@ -42,9 +50,18 @@ class ActionUnit(nn.Module):
         self.lstm = nn.LSTMCell(self.__n * 3, self.__n)
 
     def forward(self, h_caret_t, c_caret_t, u_t):
-        #assert u_t.size(1) == 1, "Only one time iteration is allowed"
+        """
+        TODO
 
-        #h_caret_t, c_caret_t = h_caret_t[:u_t.size(0), :], c_caret_t[:u_t.size(0), :]
+        :param h_caret_t:
+        :type h_caret_t:
+        :param c_caret_t:
+        :type c_caret_t:
+        :param u_t:
+        :type u_t:
+        :return:
+        :rtype:
+        """
 
         nb_ag, batch_size, hidden_size = h_caret_t.size()
 