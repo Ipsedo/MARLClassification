50a931b2aa1476fab160a85a1e30d095158b972c
Ipsedo <berriens@gmail.com>
2020-07-21 21:45:16 +0200

    use ModuleDict + specializations for MNIST & RESISC45

--- networks/models.py
+++ networks/models.py
@@ -1,108 +1,59 @@
-from networks.ft_extractor import CNN_MNIST, CNN_MNIST_2, StateToFeatures
+from networks.ft_extractor import MNISTCnn, RESISC45Cnn, StateToFeatures
 from networks.messages import MessageReceiver, MessageSender
 from networks.recurrents import BeliefUnit, ActionUnit
 from networks.policy import Policy
 from networks.prediction import Prediction
 import torch.nn as nn
 
-from typing import Optional, Dict
 
-
+#####################
+# Base class test
+#####################
 class ModelsWrapper(nn.Module):
-    map_obs: int = 0
-    map_pos: int = 1
+    map_obs: str = "b_theta_5"
+    map_pos: str = "lambda_theta_7"
 
-    decode_msg: int = 2
-    evaluate_msg: int = 3
+    decode_msg: str = "d_theta_6"
+    evaluate_msg: str = "m_theta_4"
 
-    belief_unit: int = 4
-    action_unit: int = 5
+    belief_unit: str = "belief_unit"
+    action_unit: str = "action_unit"
 
-    policy: int = 6
-    predict: int = 7
+    policy: str = "pi_theta_3"
+    predict: str = "q_theta_8"
 
-    def __init__(self, n: int, f: int, n_m: int, d: int,
-                 nb_action: int, nb_class: int,
-                 pretrained_seq_conv_cnn: Optional[nn.Module] = None) -> None:
+    def __init__(self, map_obs_module: nn.Module,
+                 n: int, n_m: int, d: int,
+                 nb_action: int, nb_class: int) -> None:
         super().__init__()
 
-        self.__b_theta_5 = CNN_MNIST(f, n)
+        self.__networks_dict = nn.ModuleDict({
+            self.map_obs: map_obs_module,
+            self.map_pos: StateToFeatures(d, n),
+            self.decode_msg: MessageReceiver(n_m, n),
+            self.evaluate_msg: MessageSender(n, n_m),
+            self.belief_unit: BeliefUnit(n),
+            self.action_unit: ActionUnit(n),
+            self.policy: Policy(nb_action, n),
+            self.predict: Prediction(n, nb_class)
+        })
 
-        if pretrained_seq_conv_cnn is not None:
-            self.__b_theta_5.seq_conv = pretrained_seq_conv_cnn
-
-        # self.__b_theta_5 = CNN_MNIST_2(f, n)
-        self.__d_theta_6 = MessageReceiver(n_m, n)
-        self.__lambda_theta_7 = StateToFeatures(d, n)
-        self.__belief_unit = BeliefUnit(n)
-        self.__m_theta_4 = MessageSender(n, n_m)
-        self.__action_unit = ActionUnit(n)
-        self.__pi_theta_3 = Policy(nb_action, n)
-        self.__q_theta_8 = Prediction(n, nb_class)
-
-        self.__op_to_module: Dict[int, nn.Module] = {
-            0: self.__b_theta_5,
-            1: self.__lambda_theta_7,
-            2: self.__d_theta_6,
-            3: self.__m_theta_4,
-            4: self.__belief_unit,
-            5: self.__action_unit,
-            6: self.__pi_theta_3,
-            7: self.__q_theta_8
-        }
-
-    def forward(self, op: int, *args):
-        return self.__op_to_module[op](*args)
+    def forward(self, op: str, *args):
+        return self.__networks_dict[op](*args)
 
 
+#####################
+# MNIST version
+#####################
+class MNISTModelWrapper(ModelsWrapper):
+    def __init__(self, f: int, n: int, n_m: int) -> None:
+        super().__init__(MNISTCnn(f, n), n, n_m, 2, 4, 10)
 
-class ModelsUnion:
-    def __init__(self, n: int, f: int, n_m: int, d: int, nb_action: int, nb_class: int, pretrained_seq_conv_cnn: nn.Sequential=None):
-        self.__b_theta_5 = CNN_MNIST(f, n)
 
-        if pretrained_seq_conv_cnn is not None:
-            self.__b_theta_5.seq_conv = pretrained_seq_conv_cnn
+#####################
+# RESISC45 version
+#####################
+class RESISC45ModelsWrapper(ModelsWrapper):
+    def __init__(self, f: int, n: int, n_m: int) -> None:
+        super().__init__(RESISC45Cnn(f, n), n, n_m, 2, 4, 45)
 
-        #self.__b_theta_5 = CNN_MNIST_2(f, n)
-        self.__d_theta_6 = MessageReceiver(n_m, n)
-        self.__lambda_theta_7 = StateToFeatures(d, n)
-        self.__belief_unit = BeliefUnit(n)
-        self.__m_theta_4 = MessageSender(n, n_m)
-        self.__action_unit = ActionUnit(n)
-        self.__pi_theta_3 = Policy(nb_action, n)
-        self.__q_theta_8 = Prediction(n, nb_class)
-
-    def map_obs(self, o_t):
-        return self.__b_theta_5(o_t)
-
-    def decode_msg(self, m_t):
-        return self.__d_theta_6(m_t)
-
-    def map_pos(self, p_t):
-        return self.__lambda_theta_7(p_t)
-
-    def belief_unit(self, h_t, c_t, u_t):
-        return self.__belief_unit(h_t, c_t, u_t)
-
-    def evaluate_msg(self, h_t_next):
-        return self.__m_theta_4(h_t_next.squeeze(0))
-
-    def action_unit(self, h_caret_t, c_caret_t, u_t):
-        return self.__action_unit(h_caret_t, c_caret_t, u_t)
-
-    def policy(self, h_caret_t_next):
-        return self.__pi_theta_3(h_caret_t_next.squeeze(0))
-
-    def predict(self, c_t):
-        return self.__q_theta_8(c_t.squeeze(0))
-
-    def get_networks(self):
-        return [self.__b_theta_5, self.__d_theta_6, self.__lambda_theta_7, self.__belief_unit,
-                self.__m_theta_4, self.__action_unit, self.__pi_theta_3, self.__q_theta_8]
-
-    def parameters(self):
-        return [p for net in self.get_networks() for p in net.parameters()]
-
-    def cuda(self):
-        for n in self.get_networks():
-            n.cuda()
