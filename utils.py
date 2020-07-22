da31233def1b0ad6fec08967b9b64ab6973ceb30
Ipsedo <berriens@gmail.com>
2020-07-22 20:12:39 +0200

    re-org options namedtuple

--- utils.py
+++ utils.py
@@ -18,20 +18,28 @@ MAOptions = NamedTuple("MAOption",
                         ("nb_action", int)])
 
 RLOptions = NamedTuple("RLOptions",
-                       [("eps", float),
-                        ("eps_decay", float),
-                        ("nb_step", int),
-                        ("nb_epoch", int),
-                        ("learning_rate", float),
+                       [("nb_step", int),
                         ("hidden_size", int),
                         ("hidden_size_msg", int),
-                        ("batch_size", int),
                         ("cuda", bool)])
+
+TrainOptions = NamedTuple("TrainOptions",
+                          [("eps", float),
+                           ("eps_decay", float),
+                           ("nb_epoch", int),
+                           ("learning_rate", float),
+                           ("batch_size", int),
+                           ("output_model_path", str)])
+
+TestOptions = NamedTuple("TestOptions",
+                         [("json_path", str),
+                          ("state_dict_path", str),
+                          ("output_img_path", str),
+                          ("nb_test_img", int)])
 
 
 def viz(agents: MultiAgent, one_img: th.Tensor,
-        max_it: int, f: int, output_dir: AnyStr,
-        color_map: Optional[AnyStr] = None) -> None:
+        max_it: int, f: int, output_dir: AnyStr) -> None:
     """
 
     :param agents:
@@ -44,29 +52,37 @@ def viz(agents: MultiAgent, one_img: th.Tensor,
     :type f:
     :param output_dir:
     :type output_dir:
-    :param color_map:
-    :type color_map:
     :return:
     :rtype:
     """
 
+    color_map = None
+
     preds, _, pos = detailed_episode(agents, one_img.unsqueeze(0).cuda(),
                                      max_it, True, 10)
+
+    one_img = one_img.permute(1, 2, 0)
+
+    h, w, c = one_img.size()
 
     img_idx = 0
 
     plt.figure()
-    plt.imshow(one_img, cmap=color_map)
+    plt.imshow(one_img.repeat(1, 1, 3), cmap=color_map)
     plt.savefig(join(output_dir, f"pred_original.png"))
 
-    curr_img = th.zeros(28, 28) - 1
+    curr_img = th.zeros(h, w, 4)
     for t in range(max_it):
 
         for i in range(len(agents)):
+            # Color
             curr_img[pos[t][i][img_idx][0]:pos[t][i][img_idx][0] + f,
-                     pos[t][i][img_idx][1]:pos[t][i][img_idx][1] + f] = \
+                     pos[t][i][img_idx][1]:pos[t][i][img_idx][1] + f, :3] = \
                 one_img[pos[t][i][img_idx][0]:pos[t][i][img_idx][0] + f,
-                        pos[t][i][img_idx][1]:pos[t][i][img_idx][1] + f]
+                        pos[t][i][img_idx][1]:pos[t][i][img_idx][1] + f, :]
+            # Alpha
+            curr_img[pos[t][i][img_idx][0]:pos[t][i][img_idx][0] + f,
+                     pos[t][i][img_idx][1]:pos[t][i][img_idx][1] + f, 3] = 1
 
         plt.figure()
         plt.imshow(curr_img, cmap=color_map)
