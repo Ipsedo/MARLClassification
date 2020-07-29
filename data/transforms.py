8248be4220c121ab178cd7c5cfd83bbda9ab98cf
Ipsedo <berriens@gmail.com>
2020-07-29 23:30:06 +0200

    ptite opti

--- data/transforms.py
+++ data/transforms.py
@@ -1,6 +1,6 @@
 import torch as th
 
-from typing import Tuple, Optional, Union
+from typing import Tuple, Optional, Union, Callable
 
 from abc import ABCMeta, abstractmethod
 
@@ -20,21 +20,26 @@ class NormalNorm(ImgTransform):
     def __init__(self, mean: Optional[Union[Tuple[float, float, float], float]] = None,
                  std: Optional[Union[Tuple[float, float, float], float]] = None):
 
-        self.__user_defined_mean = mean is not None
-        self.__user_defined_std = std is not None
+        self.__user_defined = (std is not None) and (mean is not None)
+
+        assert (std is not None) ^ (mean is not None), f"Hybrid mode not supported yet"
 
         self.__mean = th.tensor([mean] * 3) if isinstance(mean, float) else mean
         self.__std = th.tensor([std] * 3) if isinstance(std, float) else std
 
-    def __call__(self, x: th.Tensor) -> th.Tensor:
+        self.__apply_fun: Callable[[th.Tensor], th.Tensor] = self.__apply_user if self.__user_defined else self.__apply_data
 
-        mean = self.__mean if self.__user_defined_mean else x.view(3, -1).mean(dim=-1)
-        std = self.__std if self.__user_defined_std else x.view(3, -1).std(dim=-1)
+    def __apply_user(self, x: th.Tensor) -> th.Tensor:
+        return (x - self.__mean) / self.__std
 
-        mean = mean.view(3, 1, 1)
-        std = std.view(3, 1, 1)
+    def __apply_data(self, x: th.Tensor) -> th.Tensor:
+        mean = x.view(3, -1).mean(dim=0).view(3, 1, 1)
+        std = x.view(3, -1).std(dim=0).view(3, 1, 1)
 
         return (x - mean) / std
+
+    def __call__(self, x: th.Tensor) -> th.Tensor:
+        return self.__apply_fun(x)
 
     def __repr__(self):
         return self.__class__.__name__ + f"(mean = {str(self.__mean)}, std = {str(self.__std)})"
@@ -44,20 +49,26 @@ class MinMaxNorm(ImgTransform):
     def __init__(self, min_value: Optional[Union[Tuple[float, float, float], float]] = None,
                  max_value: Optional[Union[Tuple[float, float, float], float]] = None):
 
-        self.__user_defined_max = max_value is not None
-        self.__user_defined_min = min_value is not None
+        self.__user_defined = (min_value is not None) and (max_value is not None)
 
         self.__min = th.tensor([min_value] * 3) if isinstance(min_value, float) else min_value
         self.__max = th.tensor([max_value] * 3) if isinstance(max_value, float) else max_value
 
+        assert (min_value is not None) ^ (max_value is not None) == self.__user_defined, f"Hybrid mode not supported yet"
+
+        self.__apply_fun: Callable[[th.Tensor], th.Tensor] = self.__apply_user if self.__user_defined else self.__apply_data
+
+    def __apply_user(self, x: th.Tensor) -> th.Tensor:
+        return (x - self.__min) / (self.__max - self.__min)
+
+    def __apply_data(self, x: th.Tensor) -> th.Tensor:
+        max_v = x.view(3, -1).max(dim=-1)[0].view(3, 1, 1)
+        min_v = x.view(3, -1).min(dim=-1)[0].view(3, 1, 1)
+
+        return (x - min_v) / (max_v - min_v)
+
     def __call__(self, x: th.Tensor) -> th.Tensor:
-        max_value = self.__max if self.__user_defined_max else x.view(3, -1).max(dim=-1)[0]
-        min_value = self.__min if self.__user_defined_min else x.view(3, -1).min(dim=-1)[0]
-
-        min_value = min_value.view(3, 1, 1)
-        max_value = max_value.view(3, 1, 1)
-
-        return (x - min_value) / (max_value - min_value)
+        return self.__apply_fun(x)
 
     def __repr__(self):
         return self.__class__.__name__ + f"(min_value = {self.__min}, max_value = {self.__max})"
