4b2ee4a796e6402ef3da15b9c57ef1dc3f76fbbc
Ipsedo <berriens@gmail.com>
2020-08-08 11:53:17 +0200

    reformat

--- environment/observation.py
+++ environment/observation.py
@@ -15,17 +15,17 @@ def obs_img(img: th.Tensor, pos: th.Tensor, f: int) -> th.Tensor:
     :rtype:
     """
 
-    assert len(img.size()) == 4,\
+    assert len(img.size()) == 4, \
         f"images.size() == (N, C, W, H), actual = {img.size()}"
-    assert len(pos.size()) == 3,\
+    assert len(pos.size()) == 3, \
         f"Position size must be (nb_agents, batch_size, data_dim), actual = {pos.size()}"
 
     nb_a, b_pos, d = pos.size()
     b_img, c, h, w = img.size()
 
-    assert (pos[:, :, 0] + f < w).all() and (pos[:, :, 0] >= 0).all(),\
+    assert (pos[:, :, 0] + f < w).all() and (pos[:, :, 0] >= 0).all(), \
         "Unbound obervation in first dim (pos = {}, f = {}, img_size = {}!".format(pos[:, 0], f, w)
-    assert (pos[:, :, 1] + f < h).all() and (pos[:, :, 1] >= 0).all(),\
+    assert (pos[:, :, 1] + f < h).all() and (pos[:, :, 1] >= 0).all(), \
         "Unbound obervation in snd dim (pos = {}, f = {}, img_size = {}!".format(pos[:, 1], f, h)
 
     # pos.size == (nb_ag, batch_size, 2)
@@ -33,11 +33,11 @@ def obs_img(img: th.Tensor, pos: th.Tensor, f: int) -> th.Tensor:
     pos_max = pos_min + f
 
     values_x = th.arange(0, w, device=pos.device)
-    mask_x = (pos_min[:, :, 0, None] <= values_x.view(1, 1, w)) &\
+    mask_x = (pos_min[:, :, 0, None] <= values_x.view(1, 1, w)) & \
              (values_x.view(1, 1, w) < pos_max[:, :, 0, None])
 
     values_y = th.arange(0, h, device=pos.device)
-    mask_y = (pos_min[:, :, 1, None] <= values_y.view(1, 1, h)) &\
+    mask_y = (pos_min[:, :, 1, None] <= values_y.view(1, 1, h)) & \
              (values_y.view(1, 1, h) < pos_max[:, :, 1, None])
 
     mask = mask_x.unsqueeze(-2) & mask_y.unsqueeze(-1)
