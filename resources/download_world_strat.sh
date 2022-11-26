fc623c8d4f5090a39c6b11810c81f809d850fbfa
samuel <berriens@gmail.com>
2022-11-26 20:57:40 +0100

    fix worldstrat stuff

--- resources/download_world_strat.sh
+++ resources/download_world_strat.sh
@@ -6,8 +6,8 @@ if ! [[ -f "${SCRIPT_DIR}/downloaded/hr_dataset.tar.gz" ]]; then
   wget https://zenodo.org/record/6810792/files/hr_dataset.tar.gz -P "${SCRIPT_DIR}/downloaded"
 fi
 
-if ! [[ -d "${SCRIPT_DIR}/downloaded/WroldStrat" ]]; then
-  mkdir "${SCRIPT_DIR}/downloaded/WroldStrat"
-  wget https://zenodo.org/record/6810792/files/metadata.csv -P "${SCRIPT_DIR}/downloaded"
+if ! [[ -d "${SCRIPT_DIR}/downloaded/WorldStrat" ]]; then
+  mkdir "${SCRIPT_DIR}/downloaded/WorldStrat"
+  wget https://zenodo.org/record/6810792/files/metadata.csv -P "${SCRIPT_DIR}/downloaded/WorldStrat"
   tar -xvzf "${SCRIPT_DIR}/downloaded/hr_dataset.tar.gz" -C "${SCRIPT_DIR}/downloaded/WroldStrat"
 fi
