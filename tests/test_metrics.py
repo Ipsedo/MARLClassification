6ca93588229c1447480427821d8a1b1dc2b91dd3
samuel <berriens@gmail.com>
2022-11-27 03:34:25 +0100

    updated tests

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -2,7 +2,7 @@ import unittest
 
 import torch as th
 
-from marl_classification.metrics import ConfusionMeter
+from marl_classification.metrics import ConfusionMeter, LossMeter
 
 
 class TestMetrics(unittest.TestCase):
@@ -12,6 +12,8 @@ class TestMetrics(unittest.TestCase):
     def test_confusion(self) -> None:
         nb_class = int(th.randint(2, 32, (1,)).item())
         y_pred = th.eye(nb_class).to(th.float)
+
+        # error at first batch
         y_pred[0, 0] = 0.
         y_pred[0, 1] = 1.
 
@@ -27,3 +29,12 @@ class TestMetrics(unittest.TestCase):
         assert conf_mat[0, 1] == 1
 
         assert (th.diag(conf_mat)[1:] == 1).all()
+        assert conf_mat.sum() == nb_class
+
+    def test_loss(self):
+        loss_meter = LossMeter(None)
+
+        for v in [0.5, 0.25, 0.75, 0.5]:
+            loss_meter.add(v)
+
+        assert loss_meter.loss() == 0.5
