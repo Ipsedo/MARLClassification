425799ded57e718778ab89838bf347b936fd4408
samuel <berriens@gmail.com>
2022-11-30 19:22:03 +0100

    download json for fMoW dataset

--- resources/download_fMoW_rgb.sh
+++ resources/download_fMoW_rgb.sh
@@ -10,18 +10,22 @@ if ! [[ -d "${SCRIPT_DIR}/downloaded/fMoW_dataset" ]]; then
   git clone https://github.com/fMoW/dataset.git "${SCRIPT_DIR}/downloaded/fMoW_dataset"
 fi
 
+REGION="eu-west-1"
 AWS_URL="s3://spacenet-dataset/Hosted-Datasets/fmow/fmow-rgb"
 
 if ! [[ -d "${SCRIPT_DIR}/downloaded/fMoW_dataset/dataset_rgb" ]]; then
   mkdir "${SCRIPT_DIR}/downloaded/fMoW_dataset/dataset_rgb"
 
-  for cat in $( aws s3 ls --region=eu-west-1 "${AWS_URL}/train/" | perl -pe 's/.+ ([^ ]+)$/$1/' ); do
+  for cat in $( aws s3 ls --region="${REGION}" "${AWS_URL}/train/" | perl -pe 's/.+ ([^ ]+)$/$1/' ); do
     echo "${cat} will be downloaded"
     mkdir "${SCRIPT_DIR}/downloaded/fMoW_dataset/dataset_rgb/${cat}"
 
-    for folder in $( aws s3 ls --region=eu-west-1 "${AWS_URL}/train/${cat}" | perl -pe 's/.+ ([^ ]+)$/$1/' ); do
-      first_file=$( aws s3 ls --region=eu-west-1 "${AWS_URL}/train/${cat}${folder}" | grep _rgb.jpg | head -n 1 | perl -pe 's/.+ ([^ ]+)$/$1/' )
-      aws s3 cp --region=eu-west-1 "${AWS_URL}/train/${cat}${folder}${first_file}" "${SCRIPT_DIR}/downloaded/fMoW_dataset/dataset_rgb/${cat}"
+    for folder in $( aws s3 ls --region="${REGION}" "${AWS_URL}/train/${cat}" | perl -pe 's/.+ ([^ ]+)$/$1/' ); do
+      first_jpg_file=$( aws s3 ls --region="${REGION}" "${AWS_URL}/train/${cat}${folder}" | grep _rgb.jpg | head -n 1 | perl -pe 's/.+ ([^ ]+)$/$1/' )
+      first_jpg_json_file=$( aws s3 ls --region="${REGION}" "${AWS_URL}/train/${cat}${folder}" | grep _rgb.json | head -n 1 | perl -pe 's/.+ ([^ ]+)$/$1/' )
+
+      aws s3 cp --region="${REGION}" "${AWS_URL}/train/${cat}${folder}${first_jpg_file}" "${SCRIPT_DIR}/downloaded/fMoW_dataset/dataset_rgb/${cat}"
+      aws s3 cp --region="${REGION}" "${AWS_URL}/train/${cat}${folder}${first_jpg_json_file}" "${SCRIPT_DIR}/downloaded/fMoW_dataset/dataset_rgb/${cat}"
     done
   done
 fi
