0b1e29794ce5dec4f61414c205d7388b366730f0
samuel <berriens@gmail.com>
2022-12-10 02:08:26 +0100

    new test for episode

--- tests/test_episode.py
+++ tests/test_episode.py
@@ -2,10 +2,37 @@ from typing import Tuple
 
 import torch as th
 
-from marl_classification.environment import MultiAgent, detailed_episode
+from marl_classification.environment import (
+    MultiAgent,
+    detailed_episode,
+    episode,
+)
 
 
 def test_episode(
+    batch_size: int,
+    marl_m: MultiAgent,
+    step: int,
+    nb_class: int,
+    nb_agent: int,
+    dim: int,
+    height_width: Tuple[int, int],
+) -> None:
+    x = th.randn(batch_size, 1, *height_width)
+
+    pred, log_proba = episode(marl_m, x, step)
+
+    assert 3 == len(pred.size())
+    assert nb_agent == pred.size()[0]
+    assert batch_size == pred.size()[1]
+    assert nb_class == pred.size()[2]
+
+    assert 2 == len(log_proba.size())
+    assert nb_agent == log_proba.size()[0]
+    assert batch_size == log_proba.size()[1]
+
+
+def test_detailed_episode(
     batch_size: int,
     marl_m: MultiAgent,
     step: int,
